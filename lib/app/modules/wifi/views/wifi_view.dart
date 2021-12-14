89a5eb254104d44dcf0436abb0d5a1372c3e9136
renankanu <renankanu@gmail.com>
2021-12-13 21:48:00 -0300

    feature: add border to dropdown item

--- lib/app/modules/wifi/views/wifi_view.dart
+++ lib/app/modules/wifi/views/wifi_view.dart
@@ -16,34 +16,50 @@ class WifiView extends GetView<WifiController> {
             title: 'Wifi',
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 32,
+            ),
             child: Column(
               children: [
                 BaseTextField(
                   labelText: 'Nome da rede (SSID)',
                 ),
+                SizedBox(height: 20),
                 BaseTextField(
                   labelText: 'Senha',
                 ),
-                DropdownButton<String>(
-                    hint: Text(
-                      "Selecione o cliente",
-                      style: TextStyle(
-                        color: BaseColor.goldSand,
-                      ),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: BaseColor.martinique,
+                      width: 1.5,
                     ),
-                    underline: SizedBox(),
-                    iconEnabledColor: BaseColor.goldSand,
-                    items: ['teste', 'teste2']
-                        .map<DropdownMenuItem<String>>(
-                            (e) => DropdownMenuItem<String>(
-                                  value: e,
-                                  child: Text(e),
-                                ))
-                        .toList(),
-                    onChanged: (name) {
-                      log(name!);
-                    })
+                  ),
+                  child: DropdownButton<String>(
+                      hint: Text(
+                        "Selecione o cliente",
+                        style: TextStyle(
+                          color: BaseColor.goldSand,
+                        ),
+                      ),
+                      underline: SizedBox(),
+                      iconEnabledColor: BaseColor.goldSand,
+                      items: ['teste', 'teste2']
+                          .map<DropdownMenuItem<String>>(
+                              (e) => DropdownMenuItem<String>(
+                                    value: e,
+                                    child: Text(e),
+                                  ))
+                          .toList(),
+                      onChanged: (name) {
+                        log(name!);
+                      }),
+                )
               ],
             ),
           )
