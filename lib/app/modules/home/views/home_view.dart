457961097e7e629567f55b43c32617cd3accb468
renankanu <renankanu@gmail.com>
2021-12-17 20:39:50 -0300

    featuere: add animation in home screen

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 
 import '../../../routes/app_pages.dart';
@@ -14,34 +15,45 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'RKode;',
-              style: TextStyle(
-                fontSize: 42,
-                fontWeight: FontWeight.bold,
-                color: BaseColor.bunting,
+        child: AnimationLimiter(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: AnimationConfiguration.toStaggeredList(
+              duration: const Duration(milliseconds: 800),
+              childAnimationBuilder: (widget) => SlideAnimation(
+                verticalOffset: 100,
+                child: FadeInAnimation(
+                  child: widget,
+                ),
               ),
+              children: [
+                const Text(
+                  'RKode;',
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.bold,
+                    color: BaseColor.bunting,
+                  ),
+                ),
+                const SizedBox(height: 48),
+                BaseButton(
+                  icon: EvaIcons.wifi,
+                  label: 'Wifi',
+                  onPressed: () => Get.toNamed(Routes.wifi),
+                ),
+                BaseButton(
+                  icon: EvaIcons.personOutline,
+                  label: 'Vcard',
+                  onPressed: () {},
+                ),
+                BaseButton(
+                  icon: EvaIcons.navigation2Outline,
+                  label: 'Localização',
+                  onPressed: () {},
+                ),
+              ],
             ),
-            const SizedBox(height: 48),
-            BaseButton(
-              icon: EvaIcons.wifi,
-              label: 'Wifi',
-              onPressed: () => Get.toNamed(Routes.wifi),
-            ),
-            BaseButton(
-              icon: EvaIcons.personOutline,
-              label: 'Vcard',
-              onPressed: () {},
-            ),
-            BaseButton(
-              icon: EvaIcons.navigation2Outline,
-              label: 'Localização',
-              onPressed: () {},
-            ),
-          ],
+          ),
         ),
       ),
     );
